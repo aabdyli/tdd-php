ae7506002e9af8459f7ab1971c4805d6dfce6cf6
Albi Abdyli <albi.abdyli@nethex.com>
2017-10-03 17:57:55 +0200

    Leksioni 40

--- app/Thread.php
+++ app/Thread.php
@@ -50,6 +50,25 @@ class Thread extends Model
         return $this->replies()->create($reply);
     }
 
+    public function subscribe($userId = null)
+    {
+        $this->subscriptions()->create([
+            'user_id' => $userId ?: auth()->id(),
+        ]);
+    }
+
+    public function unsubscribe($userId = null)
+    {
+        $this->subscriptions()
+            ->where('user_id', $userId ?: auth()->id())
+            ->delete();
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(ThreadSubscription::class);
+    }
+    
     public function scopeFilter($query, $filters)
     {
         return $filters->apply($query);
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -49,4 +49,33 @@ class ThreadTest extends TestCase
 
         $this->assertCount(1, $this->thread->replies);
     }
+
+    /** @test */
+    public function a_thread_can_be_subscribed_to()
+    {
+        // Given we have a thread
+        // When the user subscribes to the thread
+        $this->thread->subscribe($userId = 1);
+        // Then we should be able to fetch all threads that the user has subscribed
+       $this->assertCount(
+           1,
+           $this->thread->subscriptions()->where('user_id', $userId)->get()
+        );
+    }
+
+    /** @test */
+    public function a_thread_can_be_unsubscribed_from()
+    {
+        // When the user subscribes to the thread
+        $this->thread->subscribe($userId = 1);
+        // Then we should be able to fetch all threads that the user has subscribed
+
+        $this->thread->unsubscribe($userId = 1);
+
+        $this->assertCount(
+           0,
+           $this->thread->subscriptions()->where('user_id', $userId)->get()
+        );
+
+    }
 }
